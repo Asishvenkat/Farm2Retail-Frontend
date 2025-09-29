accc0cb8ca43bc9d5a0f312d9fe179981477c225
Asish Venkat <asishvenkat.a2004@gmail.com>
2025-09-29 16:25:35 +0530

    Base URL

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:5000/api/";
+export const BASE_URL = "https://farm2retail-backend.onrender.com/api/";
 
 // Safe token extraction
 let TOKEN = null;
